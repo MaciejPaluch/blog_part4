c4ede5bac776605e493cac40d776cf88a1fca273
Maciej Paluch <maciekpaluch02@gmail.com>
2026-01-07 21:08:41 +0100

    blog_backend

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
+.then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
+app.use(express.static('dist'))
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+module.exports = app
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
